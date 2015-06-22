afd09c27ca3a59020803daaff4277931bce5972d
Icaro Camelo <icaro.camelo@hp.com>
2015-06-22 13:55:35 -0400

    Whitespace replacer

---
+++ whitespace.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#Write a method to replace all spaces in a string with ‘20’.
+
+word = 'Thanks this helped a lot!'
+caract = '%20'
+final = ''
+
+for x in word:
+	if x != ' ':
+		final += x
+	else:
+		final += caract
+
+print final
